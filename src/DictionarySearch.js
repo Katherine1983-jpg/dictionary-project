0b7f524be5e33f8b84bb24271b539dab0864cb12
Katherine1983-jpg <79195808+Katherine1983-jpg@users.noreply.github.com>
2021-10-05 19:33:57 +0700

    Added photo API and added a grid

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
   let [keyword, setKeyword] = useState(null);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +22,12 @@ export default function DictionarySearch() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000013ace0c54508949a498ae2a57a690b29c";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -36,6 +48,7 @@ export default function DictionarySearch() {
         <div className="hint">Suggested words: wine, yoga, forest, love...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
